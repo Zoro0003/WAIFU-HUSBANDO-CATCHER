d3ed946b68a61c0760dad5b8f9cd663ff87d18ab
VILLAIN <iamrkay420@gmail.com>
2024-11-25 22:08:44 +0530

    Update config.py

--- shivu/config.py
+++ shivu/config.py
@@ -5,11 +5,11 @@ class Config(object):
     OWNER_ID = "5909658683"
     sudo_users = "5909658683", "8019277081", "5608779258", "6961368696", "1881562083"
     GROUP_ID = -1002311769574
-    TOKEN = ""
+    TOKEN = "7655351916:AAHv2BuEzZe_frfK2FUGuD6THRsdQxbArd8"
     mongo_url = "mongodb+srv://TEAMBABY01:UTTAMRATHORE09@cluster0.vmjl9.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
     PHOTO_URL = ["https://files.catbox.moe/wy70cl.jpg", "https://files.catbox.moe/wy70cl.jpg"]
-    SUPPORT_CHAT = "iamvillain77"
-    UPDATE_CHAT = "oldskoolgc"
+    SUPPORT_CHAT = "WH_SUPPORT_GC"
+    UPDATE_CHAT = "iamvillain77"
     BOT_USERNAME = "Waifu_World_Robot"
     CHARA_CHANNEL_ID = "-1002311769574"
     api_id = "24061032"
