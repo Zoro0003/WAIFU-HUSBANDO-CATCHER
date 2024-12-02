79e27ea8ffa12ddc813b3f667a58f391cc6d5c5d
Zoro0003 <aryansaini77260@gmail.com>
2024-12-02 14:28:06 +0530

    Update config.py

--- shivu/config.py
+++ shivu/config.py
@@ -5,12 +5,12 @@ class Config(object):
     OWNER_ID = "5909658683"
     sudo_users = "5909658683", "8019277081", "5608779258", "6961368696", "1881562083", "8035449599"
     GROUP_ID = -1002311769574
-    TOKEN = ""
+    TOKEN = "6950971614:AAHIo1vorcKFfaw8PCkITBGfM070Z2jKCCA"
     mongo_url = "mongodb+srv://TEAMBABY01:UTTAMRATHORE09@cluster0.vmjl9.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
     PHOTO_URL = ["https://files.catbox.moe/wy70cl.jpg", "https://files.catbox.moe/wy70cl.jpg"]
     SUPPORT_CHAT = "WH_SUPPORT_GC"
     UPDATE_CHAT = "iamvillain77"
-    BOT_USERNAME = "Waifu_World_Robot"
+    BOT_USERNAME = "@Anime_catcher_bot"
     CHARA_CHANNEL_ID = "-1002311769574"
     api_id = "24061032"
     api_hash = "5ad029547f2eeb5a0b68b05d0db713be"
